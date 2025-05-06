49227d7c6ece8fe7e6e7226a6be6042410f7135b
Ericharw <ericharizki12@gmail.com>
2025-05-06 10:06:41 +0700

    add: Tugas Implementasi Eloquent Accessor pada Barang

--- app/Models/BarangModel.php
+++ app/Models/BarangModel.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class BarangModel extends Model
 {
@@ -17,11 +18,19 @@ class BarangModel extends Model
         'barang_nama',
         'harga_beli',
         'harga_jual',
-        'kategori_id'
+        'kategori_id',
+        'image'
     ];
 
     public function kategori()
     {
         return $this->belongsTo(KategoriModel::class, 'kategori_id', 'kategori_id');
+    }
+
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn($image) => url('/storage/posts/' . $image),
+        );
     }
 }
